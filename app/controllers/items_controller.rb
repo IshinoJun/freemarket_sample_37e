f0b2e03140ee6810353472955a27c17a4caf46c2
IshinoJun <t.mac.jun.0303@gmail.com>
2019-03-01 01:59:21 +0900

    Payjp機能をいったんコメントアウト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,16 +12,16 @@ class ItemsController < ApplicationController
   end
 
   def completed_purchase
-    ActiveRecord::Base.transaction do
-      require 'payjp'
-      Payjp.api_key = Rails.application.secrets.PAYJP_SECRET_KEY
-      Payjp::Charge.create(
-        amount:  @item.price,
-        card:    params['payjp-token'],
-        currency: 'jpy',
-      )
-
-    end
+    # ActiveRecord::Base.transaction do
+    #   require 'payjp'
+    #   Payjp.api_key = Rails.application.secrets.PAYJP_SECRET_KEY
+    #   Payjp::Charge.create(
+    #     amount:  @item.price,
+    #     card:    params['payjp-token'],
+    #     currency: 'jpy',
+    #   )
+    #   @item.update!(buyer_id: 1)
+    # end
   end
 
   private
